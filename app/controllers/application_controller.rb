67007ebb5f6f79d0bb209e9388f3d39842c84d80
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-26 16:46:37 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,6 +40,7 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(username: params[:username])
     
     if user && user.authenticate(params[:password])
+      session[:user_id] = user.id 
   end
 
   get "/failure" do
