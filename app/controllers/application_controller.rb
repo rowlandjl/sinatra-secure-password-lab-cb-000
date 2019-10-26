67100ba00df671418c47331bdda5b3320d67a987
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-26 16:46:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,6 +38,8 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     user = User.find_by(username: params[:username])
+    
+    if user && user.authenticate(params[:])
   end
 
   get "/failure" do
